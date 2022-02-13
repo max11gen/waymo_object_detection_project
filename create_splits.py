e117a8dd613027840b78d5f73639e9f32ef42f53
Max Elfgen <max.elfgen@gmail.com>
2022-02-13 12:21:27 +0000

    Initial commit

---
+++ create_splits.py
@@ -0,0 +1,31 @@
+import argparse
+import glob
+import os
+import random
+
+import numpy as np
+
+from utils import get_module_logger
+
+
+def split(data_dir):
+    """
+    Create three splits from the processed records. The files should be moved to new folders in the 
+    same directory. This folder should be named train, val and test.
+
+    args:
+        - data_dir [str]: data directory, /home/workspace/data/waymo
+    """
+    
+    # TODO: Split the data present in `/home/workspace/data/waymo/training_and_validation` into train and val sets.
+    # You should move the files rather than copy because of space limitations in the workspace.
+
+if __name__ == "__main__": 
+    parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
+    parser.add_argument('--data_dir', required=True,
+                        help='data directory')
+    args = parser.parse_args()
+
+    logger = get_module_logger(__name__)
+    logger.info('Creating splits...')
+    split(args.data_dir)
