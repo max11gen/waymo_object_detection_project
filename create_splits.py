8380cedcc6b8cd67e10aa094e9896feee667dd9a
Max Elfgen <max.elfgen@example.com>
2022-05-15 15:15:57 +0000

    Start work on creating splits for training

--- create_splits.py
+++ create_splits.py
@@ -16,9 +16,35 @@ def split(data_dir):
     args:
         - data_dir [str]: data directory, /home/workspace/data/waymo
     """
-    
-    # TODO: Split the data present in `/home/workspace/data/waymo/training_and_validation` into train and val sets.
-    # You should move the files rather than copy because of space limitations in the workspace.
+    source_path = data_dir + '/training_and_validation'
+    destination_paths = ['/train', '/val', '/test']
+    destination_paths = [data_dir + s for s in destination_paths]
+    total_folder_size = get_folder_size(source_path)
+    set_sizes = [0.8, 0.1, 0.1]
+    set_sizes = np.cumsum(set_sizes).tolist()
+    size_of_processed_files = 0
+    current_path_ind=0
+    for path, dirs, files in os.walk(source_path):
+        for f in files:
+            file_size = os.path.getsize(os.path.join(path, f))
+            size_of_processed_files += file_size
+            os.rename(os.path.join(path, f),os.path.join(destination_paths[current_path_ind],f))
+            if(size_of_processed_files > set_sizes[current_path_ind]*total_folder_size):
+                current_path_ind += 1
+            
+
+
+def get_folder_size(folder):
+    size = 0
+ 
+    # get size
+    for path, dirs, files in os.walk(folder):
+        for f in files:
+            fp = os.path.join(path, f)
+            size += os.path.getsize(fp)
+
+    return size        
+ 
 
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
